0b9ad9d8559baced30167cc6cc93464b586e2f65
Drithyl <hugo.bujan@gmail.com>
2021-08-26 12:19:13 -0400

    Wrapped up bcrypt in its own module to hash and check hashed passwords

---
+++ account_manager/hasher.js
@@ -0,0 +1,30 @@
+
+
+const bcrypt = require("bcrypt");
+
+const SALT_ROUNDS = 10;
+
+
+module.exports.hash = (str) =>
+{
+    return new Promise((resolve, reject) =>
+    {
+        bcrypt.hash(str, SALT_ROUNDS, (err, hash) =>
+        {
+            if (err) return reject(err);
+            else return resolve(hash);
+        });
+    });
+};
+
+module.exports.comparePasswordToHash = (str, storedHash) =>
+{
+    return new Promise((resolve, reject) =>
+    {
+        bcrypt.compare(str, storedHash, (err, result) =>
+        {
+            if (err) return reject(err);
+            else return resolve(result);
+        });
+    })
+};
