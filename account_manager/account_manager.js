69e97c6e99851d0d4d65e6f3a7019e4a3500051b
Drithyl <hugo.bujan@gmail.com>
2021-09-03 13:10:54 -0400

    Added specific error types

--- account_manager/account_manager.js
+++ account_manager/account_manager.js
@@ -1,6 +1,7 @@
 const hasher = require("./hasher");
 const logger = require("../logger/logger");
 const dbOperations = require("../database_integration/database_operations");
+const { LoginError } = require("../errors/custom_errors");
 
 
 const USERNAME_REGEX = new RegExp(/^\w|\d|_{3, 50}$/);
@@ -12,7 +13,7 @@ module.exports.logIn = async (username, password) => {
 
     if (USERNAME_REGEX.test(username) === false) {
         logger.log(`Username '${username}' failed to pass RegExp ${USERNAME_REGEX}`);
-        return Promise.reject(new Error(`The username must be between 3 and 50 characters long and can only contain letters, numbers or underscores.`));
+        return Promise.reject(new LoginError(`The username must be between 3 and 50 characters long and can only contain letters, numbers or underscores.`));
     }
 
     try {
@@ -21,7 +22,7 @@ module.exports.logIn = async (username, password) => {
 
         if (isPasswordCorrect !== true) {
             logger.log(`Password is incorrect does not match stored hash`);
-            return Promise.reject(new Error(`The password is incorrect.`));
+            return Promise.reject(new LoginError(`The password is incorrect.`));
         }
 
         logger.log(`Log in successful!`);
@@ -37,12 +38,12 @@ module.exports.signUp = async (username, password) => {
 
     if (USERNAME_REGEX.test(username) === false) {
         logger.log(`Username '${username}' failed to pass RegExp ${USERNAME_REGEX}`);
-        return Promise.reject(new Error(`The username must be between 3 and 50 characters long and can only contain letters, numbers or underscores.`));
+        return Promise.reject(new LoginError(`The username must be between 3 and 50 characters long and can only contain letters, numbers or underscores.`));
     }
 
     if (PASSWORD_REGEX.test(password) === false) {
         logger.log(`Password failed to pass RegExp ${PASSWORD_REGEX}`);
-        return Promise.reject(new Error(`The password must be between 3 and 50 characters long and can contain letters or numbers, as well as at least one special character among the following: !@#$%&*-_`));
+        return Promise.reject(new LoginError(`The password must be between 3 and 50 characters long and can contain letters or numbers, as well as at least one special character among the following: !@#$%&*-_`));
     }
 
     const hashedPassword = await hasher.hash(password);
